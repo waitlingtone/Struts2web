124b51f44a7708c74c48126f2b047c4b61ec203c
nnguyenhaidang <nnguyenhaidang@gmail.com>
2018-08-10 15:23:07 +0700

    start

---
+++ src/waitlingtone/zylphir/action/HelloAction.java
@@ -0,0 +1,16 @@
+package waitlingtone.zylphir.action;
+
+public class HelloAction {
+
+		private String name;
+		
+		public String execute() throws Exception{
+			return "success";
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+}
